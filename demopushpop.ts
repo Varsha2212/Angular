f171418fee365d3b3b3350e73b64be47bf853a29
varsha2212 <varshasindhu2020@gmail.com>
2023-11-01 12:11:19 +0530

    second commit

--- demopushpop.ts
+++ demopushpop.ts
@@ -1,7 +1,11 @@
-const myArray:number[]=[1,2,3];
+const myArray: number[]=[1,2,3];
 console.log(myArray);
-myArray.push(4);
+myArray.push(4); //push 4 to end of array
 console.log(myArray);
 myArray.pop();
 console.log(myArray);
+myArray.pop();
+console.log(myArray);
+
+//REDUCE
 
--- js/demopushpop.js
+++ js/demopushpop.js
@@ -1,7 +1,10 @@
 "use strict";
 const myArray = [1, 2, 3];
 console.log(myArray);
-myArray.push(4);
+myArray.push(4); //push 4 to end of array
 console.log(myArray);
 myArray.pop();
 console.log(myArray);
+myArray.pop();
+console.log(myArray);
+//REDUCE
